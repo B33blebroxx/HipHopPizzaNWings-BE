69b59a05fc6ea79d5a884335298c0c3363a8993d
B33blebroxx <b33blebroxx@gmail.com>
2024-04-08 16:55:35 -0500

    setup packages and secrets

--- HipHopPizzaNWings/obj/Debug/net8.0/HipHopPizzaNWings.AssemblyInfo.cs
+++ HipHopPizzaNWings/obj/Debug/net8.0/HipHopPizzaNWings.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("e3be1659-c091-4a4f-9c1d-296dff91d81a")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("HipHopPizzaNWings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9904013a4aac56b75e0e5c341d3e2a7ffc20ff75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54ee57bcdb0f2a548759c2eb7c0679c7a72c3ce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HipHopPizzaNWings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HipHopPizzaNWings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
