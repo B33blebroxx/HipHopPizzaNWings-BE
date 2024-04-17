8e0278b63c7be68698c0513113fafec5177bb56a
B33blebroxx <b33blebroxx@gmail.com>
2024-04-16 21:22:40 -0500

    Revenue

--- HipHopPizzaNWings/Models/Item.cs
+++ HipHopPizzaNWings/Models/Item.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public decimal Price { get; set; }
+        public decimal? Price { get; set; }
         public List<OrderItem> Order { get; set; } = new List<OrderItem>();
     }
 }
